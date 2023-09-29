5c71d56fa9ed482a3aeb5d7490cc6b1664f650b6
nalbam <me@nalbam.com>
2023-09-29 05:41:04 +0000

    add contents

---
+++ content/submit/_index.md
@@ -0,0 +1,7 @@
+---
+archetype: "chapter"
+title: "Submit"
+weight: 3
+---
+
+Lorem Ipsum.
--- docs/index.search.js
+++ docs/index.search.js
@@ -1,6 +1,13 @@
 var relearn_search_index=[{breadcrumb:"Home",content:`Discover what this Hugo theme is all about and the core-concepts behind it.
-`,description:"",tags:null,title:"Basics",uri:"/deepracer-workshop/basics/index.html"},{breadcrumb:"Home",content:"",description:"",tags:null,title:"카테고리",uri:"/deepracer-workshop/categories/index.html"},{breadcrumb:"Home",content:"Contributors Tools Hugo Hugo Relearn Theme ",description:"",tags:null,title:"Credits",uri:"/deepracer-workshop/credits/index.html"},{breadcrumb:"",content:`DeepRacer Workshop 주 A notice disclaimer
+`,description:"",tags:null,title:"Basics",uri:"/deepracer-workshop/basics/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"15 Min",uri:"/deepracer-workshop/15-min/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"Submit",uri:"/deepracer-workshop/submit/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"Sagemaker Notebook",uri:"/deepracer-workshop/notebook/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"RaceLine_Calculation",uri:"/deepracer-workshop/calculation/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"RaceLine_ActionSpace",uri:"/deepracer-workshop/actionspace/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"Capstone reward function",uri:"/deepracer-workshop/capstone/index.html"},{breadcrumb:"Home",content:`Lorem Ipsum.
+`,description:"",tags:null,title:"Analysis_ActionSpace",uri:"/deepracer-workshop/analysis/index.html"},{breadcrumb:"Home",content:"",description:"",tags:null,title:"카테고리",uri:"/deepracer-workshop/categories/index.html"},{breadcrumb:"Home",content:"Contributors Tools Hugo Hugo Relearn Theme ",description:"",tags:null,title:"Credits",uri:"/deepracer-workshop/credits/index.html"},{breadcrumb:"",content:`DeepRacer Workshop 주 A notice disclaimer
 참고 An information disclaimer
 도움말 A tip disclaimer
 주의 A warning disclaimer
-Topics Basics Credits `,description:"",tags:null,title:"Home",uri:"/deepracer-workshop/index.html"},{breadcrumb:"Home",content:"",description:"",tags:null,title:"태그",uri:"/deepracer-workshop/tags/index.html"}]
+Topics Basics 15 Min Submit Sagemaker Notebook RaceLine_Calculation RaceLine_ActionSpace Capstone reward function Analysis_ActionSpace Credits `,description:"",tags:null,title:"Home",uri:"/deepracer-workshop/index.html"},{breadcrumb:"Home",content:"",description:"",tags:null,title:"태그",uri:"/deepracer-workshop/tags/index.html"}]
