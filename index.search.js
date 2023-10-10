5174dc5a30d5cf2c1ea7dda0999b7dfef37bf438
nalbam <nalbam@users.noreply.github.com>
2023-10-10 10:07:49 +0000

    deploy: 5fb668ac5e3a255e0f1aab6a6adfb34fc6b56ff5

--- index.search.js
+++ index.search.js
@@ -108,6 +108,7 @@ Trainning 200분 훈련을 완료하였습니다. 아마도 다음과 같은 그
 `,description:"",tags:null,title:"최적화 보상 함수",uri:"/deepracer-workshop/reward_function/index.html"},{breadcrumb:"Home",content:`Download logs 분석 하고자 하는 모델 에서 Download logs 를 클릭하여 주행 로그를 다운로드 합니다.
 Analysis ActionSpace 주피터 노트북에서 Analysis_ActionSpace.ipynb 노트북을 클릭합니다.
 Kernel은 coda_python3 를 선택하고, 노트북을 Trust 합니다.
+처음에는 사용하는 라이브러리가 설치되어있지 않습니다. 첫 셀의 주석을 풀고 ▶️ Run 을 실행합니다.
 Run all cells 아래 셀에서 track_name = reInvent2019_track_ccw 를 확인 합니다.
 fname = logs/training-robomaker.log 를 확인 합니다.
 ⏩ 버튼을 클릭하면 전체 셀이 실행됩니다.
